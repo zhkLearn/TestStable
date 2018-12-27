c8a1a89cf81cf60b72f02bfe6b32ee746f240cce
张昆 <zhk@users.noreply.github.com>
2018-12-27 17:28:24 +0800

    Update testRaw.lua

--- Lua/testRaw.lua
+++ Lua/testRaw.lua
@@ -1,33 +1,27 @@
 print("\n1:")
 
 --local sraw = require "StableRaw"
-
 sraw.init()  -- init lightuserdata metatable
+
+
 print("\n2:")
 
 local t = sraw.create()
+
+
 print("\n3:")
 
---local t2 = sraw.create()
---sraw.set(t2, "world", true)
---sraw.set(t, "hello", t2)
-
 local subT = {1, 2, 3}
-
 t.hello = {world = true, "one", "two", 3, False = false, testT = {true, 3, testttt = {"Alpha", nil, 6}}} 
--- If you don't want to set explicit, use this :
---  sraw.set(t,"hello", { world = true })
-print("\n4:")
 
-t.subTable = subT
--- print(t.hello.world)
--- or use this :
--- hello = sraw.get(t,"hello") ; print( sraw.get(hello, "world"))
+
+print("\n4:")
 
 -- you can send t (a lightuserdata) to other lua state. (thread safe)
 
-
+t.subT = subT
 sraw.dump(t)
+
 print("\n5:")
 
 function dumpSTable(tbl)
@@ -43,6 +37,9 @@ end
 
 dumpSTable(t)
 
+sraw.share(t)
+
+local fromC = sraw.acquire("tname")
+dumpSTable(fromC)
 
 print("\n6:")
-
